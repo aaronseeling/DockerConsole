3ea6bed799c9ce5fe9f53384e8718eef965967bf
Aaron Seeling <aaron.seeling@bisoftwaresolutions.com>
2018-12-18 16:32:27 -0600

    Commit

--- DockerConsole/Program.cs
+++ DockerConsole/Program.cs
@@ -6,7 +6,15 @@ namespace DockerConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double sum = 0.0;
+            double i = 0.0;
+            while (1 == 1)
+            {
+                sum += 4.0 * Math.Pow(-1.0, i) / (2.0 * i + 1);
+                if (i % 10000000 == 0)
+                    Console.WriteLine("{0}      {1}", DateTime.Now, sum);
+                i++;
+            }
         }
     }
 }
